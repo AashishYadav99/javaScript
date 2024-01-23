b307196175ada371e722f908273e9471da7e67e7
AashishYadav99 <135540646+AashishYadav99@users.noreply.github.com>
2024-01-23 05:44:44 +0000

    ternary basics...

---
+++ JavaScript/ternary_operator.js
@@ -0,0 +1,13 @@
+// ternary operator is used to perform operation in single line 
+//or we can say its reduce line of codes
+
+const age = 28;
+let message;
+console.log(age>18 ? message = "adult" : message = "not a adult");
+
+// 
+
+let age1 = 25;
+let mesaage ;
+message1=age1>18 ? message="can vote" : message ="can't vote";
+console.log(message1);

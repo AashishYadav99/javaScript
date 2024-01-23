0f691c0abf9f4a512d140482aeca14d76e3da3e9
AashishYadav99 <135540646+AashishYadav99@users.noreply.github.com>
2024-01-23 05:21:40 +0000

    string basic with methods..

---
+++ .devcontainer/JavaScript/string.js
@@ -0,0 +1,57 @@
+// const name="ashish yadav";
+// console.log(name);
+// const userAccount={
+// fullName:"Yadav",
+// age : 26,
+// Address : "Lucknow"
+// }
+
+// console.log(userAccount);
+// console.log(name.length);
+
+
+// converting the string into number
+/*const num="9";
+console.log(typeof(num));
+console.log(typeof(+num));
+*/
+
+
+// convertimg the number into string
+
+// const x = 9
+// console.log(typeof(x));
+// console.log(typeof(x.toString));
+
+// console.log(typeof(x+""));
+
+
+
+// javaScript String method....
+
+//  1-- trim -> method is used to removed the extra spaces from string....
+
+const add="    Lucknow    "
+console.log(add);
+console.log(add.trim());
+
+
+// 2--charAt is used for finding the index number of any specific charatcter...
+
+console.log(add.charAt(8));
+
+
+//3 -- toUpperCase and LowerCase
+
+console.log(add.toUpperCase());
+
+//4--- slice method -> The slice() method extracts and returns a section of a string.
+
+const fullName = "Ashish Kumar Yadav";
+console.log(fullName.slice(0,3));
+console.log(fullName);
+
+
+// subString method
+
+console.log(fullName.substring(0,6));
